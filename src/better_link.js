3ffd9fd2adc6c97abeda1246d3d08098926a35de
LuckyWind_sck <luckywindsck@gmail.com>
2020-06-14 17:42:46 +0900

    style: change indentation from tab to two spaces

--- src/better_link.js
+++ src/better_link.js
@@ -1,21 +1,21 @@
 const get_sfsid = () => {
-	return new Promise((resolve, reject) => {
-		chrome.storage.local.get(['sfsid'], function(result) {
-			if(result.sfsid){
-				resolve(result.sfsid);
-			} else {
-				reject();
-			}
-		});
-	});
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['sfsid'], function(result) {
+      if(result.sfsid){
+        resolve(result.sfsid);
+      } else {
+        reject();
+      }
+    });
+  });
 }
 get_sfsid().then(
-	response => {
-		document.body.innerHTML = document.body.innerHTML
-		.replace(/<a href="\/sfc-sfs\/"><img/, '<a href="https://vu.sfc.keio.ac.jp/sfc-sfs/portal_s/s01.cgi?id=' + response + '&type=s&mode=1"><img')
-		.replace(/(href="https:\/\/vu\.sfc\.keio\.ac\.jp\/sfc-sfs\/.*?") target="_blank"/g, "$1 target=\"_top\"")
-		.replace(/(href="(?!https?:\/\/).*?") target="_blank"/g, "$1")
-		.replace(/ target="(?!_).*?"/g, "")
-		.replace(/ target="_f_new"/g, "");
-	}
+  response => {
+    document.body.innerHTML = document.body.innerHTML
+    .replace(/<a href="\/sfc-sfs\/"><img/, '<a href="https://vu.sfc.keio.ac.jp/sfc-sfs/portal_s/s01.cgi?id=' + response + '&type=s&mode=1"><img')
+    .replace(/(href="https:\/\/vu\.sfc\.keio\.ac\.jp\/sfc-sfs\/.*?") target="_blank"/g, "$1 target=\"_top\"")
+    .replace(/(href="(?!https?:\/\/).*?") target="_blank"/g, "$1")
+    .replace(/ target="(?!_).*?"/g, "")
+    .replace(/ target="_f_new"/g, "");
+  }
 )
